98d325b31b79b9b4583418c328d69303d6b266d5
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2023-12-17 18:08:55 +0700

    Create base layout

---
+++ ImplementingModernCollectionViews/ListViewController.swift
@@ -0,0 +1,136 @@
+import UIKit
+import SwiftUI
+
+class MyCollectionViewController: UIViewController {
+    
+    private var collectionView: UICollectionView!
+    private var layout: UICollectionViewLayout!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "My List"
+        
+        setupList()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        setupListOrientation()
+    }
+    
+    private func setupListOrientation() {
+        guard
+            let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+            let sceneDelegate = windowScene.delegate as? SceneDelegate else {
+            return
+        }
+        
+        let orientation = sceneDelegate.window?.windowScene?.interfaceOrientation ?? .portrait
+        layout = orientation.isPortrait || orientation == .unknown
+        ? createSingleColumnLayout()
+        : createTwoColumnLayout()
+        
+        collectionView.setCollectionViewLayout(layout, animated: true)
+    }
+    
+    private func setupList() {
+        layout = createSingleColumnLayout()
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    private func createSingleColumnLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.5))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, repeatingSubitem: item, count: 2)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+    
+    private func createTwoColumnLayout() -> UICollectionViewLayout {
+        // Item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+        
+        // Group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(100))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item, item])
+        group.interItemSpacing = .fixed(5)
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        
+        // Layout
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+}
+
+extension MyCollectionViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+}
+
+extension MyCollectionViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("item :\(indexPath)")
+    }
+}
+
+struct CollectionViewPreview: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> MyCollectionViewController {
+        MyCollectionViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: MyCollectionViewController, context: Context) { }
+}
+
+struct ContentView: View {
+    var body: some View {
+        CollectionViewPreview()
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+            ContentView()
+                .previewInterfaceOrientation(.landscapeLeft)
+        }
+    }
+}
--- ImplementingModernCollectionViews/ViewController.swift
+++ ImplementingModernCollectionViews/ViewController.swift
@@ -1,19 +1,16 @@
-//
-//  ViewController.swift
-//  ImplementingModernCollectionViews
-//
-//  Created by arifin on 17/12/23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        
+        DispatchQueue.main.asyncAfter(deadline: .now(), execute: {
+            let controller = UINavigationController(rootViewController: MyCollectionViewController())
+            controller.modalPresentationStyle = .fullScreen
+            self.present(controller, animated: true)
+        })
     }
-
-
 }
 
